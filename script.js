ec3702e55e85fa526b9a598bbf873ed35d96bbae
HellenTamara <hellen.tamara@outlook.com>
2023-06-13 18:19:00 +0900

    resizing-tablet-size

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ $(document).ready(function () {
     $(".arrow-wrap-1").click(function () {
         window.open("https://example.com", "_blank");
     });
-});
 
-$(document).ready(function () {
     $(".arrow-wrap-2").click(function () {
         window.open("https://example.com", "_blank");
     });
@@ -31,4 +29,31 @@ window.addEventListener('scroll', () => {
         stopAnimation();
     }
 });
+
+
+// Contact window
+$(document).ready(function () {
+    $(".button-style").click(function (e) {
+        e.stopPropagation();
+        $(".window-overlay").fadeIn();
+        $(".window-wrapper").addClass("change");
+    });
+
+    $(window).click(function () {
+        if ($(".window-wrapper").hasClass("change")) {
+            $(".window-overlay").fadeOut();
+            $(".window-wrapper").removeClass("change");
+        }
+    });
+
+    $(".window-wrapper").click(function (e) {
+        e.stopPropagation();
+    });
+});
+
+
+
+
+
+
 
