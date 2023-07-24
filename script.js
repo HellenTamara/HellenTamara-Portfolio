ecb00c36e64c852ef37ab0c9f32e58942ed69c1a
HellenTamara <hellen.tamara@outlook.com>
2023-07-24 15:46:43 +0900

    language feature fix

--- script.js
+++ script.js
@@ -80,21 +80,6 @@ $(document).ready(function () {
     });
 });
 
-// Language 
-$(document).ready(function () {
-    $(".language-block").click(function (e) {
-        e.stopPropagation();
-        const lang = $(".lang-pt").css("display");
-        if (lang == "none") {
-            $(".lang-pt").css("display", "block");
-            $(".lang-eng").css("display", "none");
-        } else {
-            $(".lang-pt").css("display", "none");
-            $(".lang-eng").css("display", "block");
-        }
-    });
-});
-
 // I'm feeling lucky button
 
 $(document).ready(function () {
