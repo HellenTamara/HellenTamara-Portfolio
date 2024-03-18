9e0d777ae582444dff4f8b59345b2c6b1b1b4120
Hellen Tamara <hellen.tamara@outlook.com>
2024-03-19 00:48:41 +0900

    Portuguese versions added

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ $(document).ready(function () {
 // Projects Redirecting Links
 $(document).ready(function () {
 
+  // English Version - mobile
   $('.project1').on('click', function () {
     document.location = `project1page.html`;
   });
@@ -75,16 +76,26 @@ $(document).ready(function () {
     document.location = `project3page.html`;
   });
 
+  // Portuguese Version - mobile
+  $('.project1.lang-pt').on('click', function () {
+    document.location = `project1pt.html`;
+  });
+
+  $('.project2.lang-pt').on('click', function () {
+    document.location = `project2pt.html`;
+  });
+
+  $('.project3.lang-pt').on('click', function () {
+    document.location = `project3pt.html`;
+  });
+
+  // English Version - pc
   $('.arrow-wrap-1').on('click', function () {
     const firstChildClasses = $('.project-background.project-1').children().first().attr('class');
     const match = firstChildClasses.match(/project(\d+)/);
     const projectNumber = parseInt(match[1]);
     document.location = `project${projectNumber}page.html`;
   });
-
-  // $('.project1.lang-pt, .arrow-wrap-1.lang-pt').on('click', function () {
-  //   document.location = "project1pt.html";
-  // });
 
   $('.arrow-wrap-2').on('click', function () {
     const firstChildClasses = $('.project-background.project-2').children().first().attr('class');
@@ -93,10 +104,6 @@ $(document).ready(function () {
     document.location = `project${projectNumber}page.html`;
   });
 
-  // $('.project2.lang-pt, .arrow-wrap-2.lang-pt').on('click', function () {
-  //   document.location = "project2pt.html";
-  // });
-
   $('.arrow-wrap-3').on('click', function () {
     const firstChildClasses = $('.project-background.project-3').children().first().attr('class');
     const match = firstChildClasses.match(/project(\d+)/);
@@ -104,17 +111,49 @@ $(document).ready(function () {
     document.location = `project${projectNumber}page.html`;
   });
 
-  // $('.project3.lang-pt, .arrow-wrap-3.lang-pt').on('click', function () {
-  //   document.location = "project1pt.html";
-  // });
+  // Portuguese Version - pc
+  $('.arrow-wrap-1.lang-pt').on('click', function () {
+    const firstChildClasses = $('.project-background.project-1.lang-pt').children().first().attr('class');
+    const match = firstChildClasses.match(/project(\d+)/);
+    const projectNumber = parseInt(match[1]);
+    document.location = `project${projectNumber}pt.html`;
+  });
+
+
+  $('.arrow-wrap-2.lang-pt').on('click', function () {
+    const firstChildClasses = $('.project-background.project-2.lang-pt').children().first().attr('class');
+    const match = firstChildClasses.match(/project(\d+)/);
+    const projectNumber = parseInt(match[1]);
+    document.location = `project${projectNumber}pt.html`;
+  });
+
+
+  $('.arrow-wrap-3.lang-pt').on('click', function () {
+    const firstChildClasses = $('.project-background.project-3.lang-pt').children().first().attr('class');
+    const match = firstChildClasses.match(/project(\d+)/);
+    const projectNumber = parseInt(match[1]);
+    document.location = `project${projectNumber}pt.html`;
+  });
 });
 
 // I'm feeling lucky button
 
 $(document).ready(function () {
+  // English
   $(".lucky-btn").click(function () {
     let number = 1 + Math.floor(Math.random() * 3);
     let randomLink = `.arrow-wrap-${number}`;
+    let linkElement = $(randomLink);
+
+    if (linkElement.length) {
+      linkElement[0].click();
+    }
+  });
+
+  // Portuguese
+  $(".lucky-btn.lang-pt").click(function () {
+    let number = 1 + Math.floor(Math.random() * 3);
+    let randomLink = `.arrow-wrap-${number}.lang-pt`;
     let linkElement = $(randomLink);
 
     if (linkElement.length) {
